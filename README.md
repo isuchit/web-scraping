03f271684c774f6ae66229d81183962e740ccb7d
ROTTI <srotti@dxc.com>
2020-12-26 01:13:13 +0530

    web-scraping api

---
+++ README.md
@@ -0,0 +1,35 @@
+# Web Scraping
+
+A Web scraping API which accepts a URL
+
+API Endpoint 👉 https://api.suchitrotti.com/scrape/
+
+Body
+
+```json
+{
+    "url":"https://github.com/isuchit"
+}
+```
+
+Response
+
+```json
+{
+    "title": "isuchit (Suchit Rotti) · GitHub",
+    "description": "DevOps | AWS ☁ | React | Full Stack Developer </>. isuchit has 17 repositories available. Follow their code on GitHub.",
+    "images": [
+        "https://github.githubassets.com/images/search-key-slash.svg",
+        "",
+        "",
+        "",
+        "https://avatars2.githubusercontent.com/u/26110025?s=88&u=4003ef1272faa04b985f4a4890e4f1b3bfb325aa&v=4",
+        "https://avatars2.githubusercontent.com/u/26110025?s=88&u=4003ef1272faa04b985f4a4890e4f1b3bfb325aa&v=4",
+        "https://avatars3.githubusercontent.com/u/26110025?s=460&u=4003ef1272faa04b985f4a4890e4f1b3bfb325aa&v=4"
+    ],
+    "ogtitle": "isuchit - Overview",
+    "ogtype": "profile",
+    "ogimages": "https://avatars3.githubusercontent.com/u/26110025?s=400&u=4003ef1272faa04b985f4a4890e4f1b3bfb325aa&v=4",
+    "ogurl": "https://github.com/isuchit"
+}
+```
---
+++ functions/scrape-metadata/index.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+let webScrape = async (event, context) => {
+  return await new Promise(async (resolve, reject) => {
+    try {
+      console.log("event:", JSON.stringify(event));
+      console.log("context:", JSON.stringify(context));
+
+      let htmlUrl = JSON.parse(event.body).url;
+      let response = await axios.get(htmlUrl);
+
+      let metadata = {};
+      let $ = cheerio.load(response.data);
+      let ogTitle = $("head title").text();
+      let ogDescription = $('meta[name="description"]').attr("content");
+      let ogImages = $("img");
+      let ogKeywords = $('meta[name="keywords"]').attr("content");
+
+      let ogtitle = $('meta[property="og:title"]').attr("content");
+      let ogtype = $('meta[property="og:type"]').attr("content");
+      let ogimages = $('meta[property="og:image"]').attr("content");
+      let ogurl = $('meta[property="og:url"]').attr("content");
+
+      if (ogTitle) {
+        metadata.title = ogTitle;
+      }
+
+      if (ogDescription) {
+        metadata.description = ogDescription;
+      }
+
+      if (ogKeywords) {
+        metadata.keywords = ogKeywords;
+      }
+
+      if (ogImages && ogImages.length) {
+        metadata.images = [];
+        for (let i = 0; i < ogImages.length; i++) {
+          metadata.images.push($(ogImages[i]).attr("src"));
+        }
+      }
+
+      if (ogtitle && ogtitle.length) {
+        metadata.ogtitle = ogtitle;
+      }
+
+      if (ogtype && ogtype.length) {
+        metadata.ogtype = ogtype;
+      }
+
+      if (ogimages && ogimages.length) {
+        metadata.ogimages = ogimages;
+      }
+
+      if (ogurl && ogurl.length) {
+        metadata.ogurl = ogurl;
+      }
+
+      return resolve(prepareResponse(200, metadata));
+    } catch (error) {
+      console.error("Error: ", error);
+      return resolve(prepareResponse(500));
+    }
+  });
+};
+
+function prepareResponse(statusCode, message) {
+    return {
+        "statusCode": statusCode,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST,GET,OPTIONS'
+        },
+        "body": JSON.stringify(message ? message : {}),
+        "isBase64Encoded": false,
+    };
+}
+
+module.exports = {
+  handler: webScrape,
+};
